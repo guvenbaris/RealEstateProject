f9b281d110c5dc7269da09dd98544653b1a3b05f
Güven Barış ÇAKAN <gvnbrs54@gmail.com>
2022-01-20 23:58:46 +0300

    Bootstrap added

---
+++ RealEstateWebApp/Controllers/AdvertisimentLandController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RealEstateWebApp.DataAccess;
+using RealEstateWebApp.Models;
+
+namespace RealEstateWebApp.Controllers
+{
+    public class AdvertisimentLandController : Controller
+    {
+        private AdvertisementLandDal _advertisementLandDal =
+            new AdvertisementLandDal(new LandDal(new AddressDal()), new UserDal(new AddressDal()));
+
+        // GET: AdvertisimentLand
+        public ActionResult Index()
+        {
+            List<AdvertisimentLand> advertisimentLands =  _advertisementLandDal.GetAll();
+
+            return View(advertisimentLands);
+        }
+
+        // GET: AdvertisimentLand/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AdvertisimentLand/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AdvertisimentLand/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AdvertisimentLand/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AdvertisimentLand/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AdvertisimentLand/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AdvertisimentLand/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ RealEstateWebApp/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RealEstateWebApp.DataAccess;
+using RealEstateWebApp.Models;
+
+namespace RealEstateWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private AdvertisementLandDal _advertisementLandDal =
+            new AdvertisementLandDal(new LandDal(new AddressDal()), new UserDal(new AddressDal()));
+
+        private AdvertisementCommercialDal _advertisementCommercialDal =
+            new AdvertisementCommercialDal(new UserDal(new AddressDal()), new CommercialDal(new AddressDal()));
+
+        private AdvirtisementResidentialDal _advirtisementResidentialDal =
+            new AdvirtisementResidentialDal(new ResidentialDal(new AddressDal()), new UserDal(new AddressDal()));
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult LandPartial()
+        {
+            List<AdvertisimentLand> residentials = _advertisementLandDal.GetAll();
+            return View(residentials);
+        }
+       
+    }
+}
