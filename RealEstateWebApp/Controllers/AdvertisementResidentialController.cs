c176a0d7a8341223f091b0cde63acc98587b5d8b
Güven Barış ÇAKAN <gvnbrs54@gmail.com>
2022-01-23 23:02:37 +0300

    ResidentialController added.

---
+++ RealEstateWebApp/Controllers/AdvertisementResidentialController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RealEstateWebApp.DataAccess;
+using RealEstateWebApp.Models;
+
+namespace RealEstateWebApp.Controllers
+{
+    public class AdvertisementResidentialController : Controller
+    {
+
+
+        private readonly AdvirtisementResidentialDal _advirtisementResidentialDal =
+            new AdvirtisementResidentialDal(new ResidentialDal(new AddressDal()), new UserDal(new AddressDal()));
+
+
+        // GET: AdvertisementResidential
+        public ActionResult Index()
+        {
+
+            List<AdvertisimentResedential> advertisimentResedentials = _advirtisementResidentialDal.GetAll();
+            return View(advertisimentResedentials);
+        }
+    }
+}
---
+++ RealEstateWebApp/Controllers/AdvertisimentCommercialController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RealEstateWebApp.DataAccess;
+using RealEstateWebApp.Models;
+
+namespace RealEstateWebApp.Controllers
+{
+    public class AdvertisimentCommercialController : Controller
+    {
+        private AdvertisementCommercialDal _advertisementCommercialDal =
+            new AdvertisementCommercialDal(new UserDal(new AddressDal()), new CommercialDal(new AddressDal()));
+
+        // GET: AdvertisimentCommercial
+        public ActionResult Index()
+        {
+
+            List<AdvertisimentCommercial> advertisiments = _advertisementCommercialDal.GetAll();
+            return View(advertisiments);
+        }
+    }
+}
